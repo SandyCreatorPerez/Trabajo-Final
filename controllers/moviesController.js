dabeab05c065528c5e013068dc696038a724b2d5
SandyCreatorPerez <perezsandy775@gmail.com>
2021-02-02 00:14:05 -0300

    2

---
+++ controllers/moviesController.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+let moviesController = {
+    listar: (req,res)=>{
+        res.render ('movies')
+    },
+    detalle: (req,res)=>{
+        res.render('detalle')
+    },
+    nueva: (req,res)=>{
+        res.render('nueva'); 
+    },
+    recomendadas: (req,res)=>{
+        res.render('recomendadas')
+    },
+    buscar: (req,res)=>{
+        res.rebder('buscar')
+    }
+}
+module.exports = moviesController; 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var moviesController = require ('../controllers/moviesController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/movies',moviesController.listar); 
+router.get('/movies/detail/:id',moviesController.detalle);
+router.get ('/movies/new',moviesController.nueva); 
+router.get ('/movies/recommended',moviesController.recomendadas);
+router.post('movies/search',moviesController.buscar); 
 module.exports = router;
